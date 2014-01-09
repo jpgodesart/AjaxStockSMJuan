3a26e8b32e5e2834e26a0ecb823a8a3d2724c584
al037721 <al037721@s09-pc18>
2014-01-09 09:07:58 +0100

    2

---
+++ src/main/java/net/rafaelaznar/dao/ProductoDao.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.rafaelaznar.dao;
+
+import net.rafaelaznar.bean.ProductoBean;
+
+/**
+ *
+ */
+public interface ProductoDao extends GenericDao<ProductoBean> {
+    
+}
